cb0aa512ce764f96a92f1e117f8ae2f04c66d709
mayankagarwal44442 <mayankagarwal44442@gmail.com>
2020-02-03 10:11:29 +0530

    Finished boiler plate code

--- Contacts/index.js
+++ Contacts/index.js
@@ -1,8 +1,37 @@
 var express = require('express');
+var cors = require('cors');
+var mysql = require('mysql')
+var url = require('url')
+
 var app = express();
 
+
+app.use(cors());
+
+
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '....',
+	database: 'reactdb'
+});
+
+connection.connect();
+
+
+
 app.get('/', function(req, res){
-   res.send("Hello world!");
+
+	var q = url.parse(req.url, true);
+	var get_params = q.query;
+
+	connection.query("select * from contacts where id=" + get_params.id , function(err, rows, fields){
+		if(err) throw err;
+		console.log(JSON.stringify(rows[0]));
+		res.send(JSON.stringify(rows[0]));
+	}); 	
+
+
 });
 
 app.listen(3000);
