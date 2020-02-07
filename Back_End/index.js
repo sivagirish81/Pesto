69a5a792f69404ae0bb9e2e694b851406719ff61
mayankagarwal44442 <mayankagarwal44442@gmail.com>
2020-02-07 10:11:29 +0530

    Finished sql. Set up dev env

---
+++ Back_End/index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var cors = require('cors');
+var url = require('url');
+var mysql = require('mysql');
+var app = express();
+app.use(cors());
+
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '....',
+	database: 'pesto'
+});
+
+connection.connect();
+
+
+
+app.get('/', function (req, res) {
+    console.log("Received Request");
+    let q = url.parse(req.url, true);
+    var qdata = q.query;
+    console.log(qdata.name);
+
+	connection.query("select pestos from user_pestos where username=\"" + qdata.name + "\"" , function(err, rows, fields){
+		if(err) throw err;
+		console.log(JSON.stringify(rows[0]));
+		res.send(JSON.stringify(rows[0]));
+	}); 
+
+;})
+
+
+var server = app.listen(3000, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("app listening at http://%s:%s", host, port);
+})
---
+++ Back_End/pesto.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE pesto;
+
+create table user_pestos( 
+     username VARCHAR(30) NOT NULL,
+     pestos VARCHAR(3000)
+     )
+     ;
+
+insert into user_pestos values("mayank", "I met Nick; I met Joe; I met Kevin;");
+
+
