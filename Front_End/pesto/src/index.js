f0e939d5d5f7e02259be4daabaee45a64ddaef39
mayankagarwal44442 <mayankagarwal44442@gmail.com>
2020-02-07 12:31:02 +0530

    added Routes

--- Front_End/pesto/src/index.js
+++ Front_End/pesto/src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+        <Router>
+            <Route path = "/" component={App} />
+        </Router>
+        , document.getElementById('root')
+    );
 
 
 serviceWorker.unregister();
