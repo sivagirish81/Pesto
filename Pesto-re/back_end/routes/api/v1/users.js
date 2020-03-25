90c0d2475ddcd06f0dbfc4acca2bf9905f1d3290
Siva Girish <sivagirish81@gmail.com>
2020-03-25 17:58:08 +0530

    Login init

---
+++ Pesto-re/back_end/routes/api/v1/users.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+// Load input validation
+const validateRegisterInput = require("../../../utils/register");
+const validateLoginInput = require("../../../utils/login");
+// Load User model
+const User = require("../../../models/User");
+
+router.post("/register", (req,res) => {
+    // Checking whether details provided are valid or not
+    const { errors, isValid } = validateRegisterInput(req.body);
+    if (!isValid)
+        return res.status(400).json(errors);
+
+    // Updating the database
+    User.findOne({email : req.body.email}).then(user=> {
+        if (user)
+            return res.status(400).json({email : "Email Aldready exists"});
+        else
+        {
+            const newUser = new User({
+                name : req.body.name,
+                email : req.body.email,
+                password : req.body.password
+            });
+            // Hashing passwords
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                  if (err) throw err;
+                  newUser.password = hash;
+                  newUser
+                    .save()
+                    .then(user => res.json(user))
+                    .catch(err => console.log(err));
+                });
+            });
+        }
+    });
+});
+
+router.post("/login", (req,res) => {
+    // Checking whether details provided are valid or not
+    const { errors, isValid } = validateLoginInput(req.body);
+    if (!isValid)
+        return res.status(400).json(errors);
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({email}).then(user => {
+        if (!user)
+            return res.status(400).json({InvalidEmail: "Email not found"});
+        // Check password
+        bcrypt.compare(password, user.password).then(isMatch => {
+            if (isMatch) {
+              // User matched
+              // Create JWT Payload
+              const payload = {
+                id: user.id,
+                name: user.name
+              };
+        // Sign token
+              jwt.sign(
+                payload,
+                config.get("secretOrKey"),
+                {
+                  expiresIn: 31556926 // 1 year in seconds
+                },
+                (err, token) => {
+                  res.json({
+                    success: true,
+                    token: "Bearer " + token
+                  });
+                }
+              );
+            } else {
+              return res
+                .status(400)
+                .json({ passwordincorrect: "Password incorrect" });
+            }
+        });
+    });
+});
+
+module.exports = router;
---
+++ Pesto-re/back_end/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const passport = require("passport");
+const users = require("./routes/api/v1/users");
+const app = express();
+
+const connectDB = require("./config/db");
+
+// connect to MongoDB
+connectDB();
+
+// Init Body Parser middleware
+app.use(express.json({ extended: false }));
+
+const PORT = 5000;
+//allows usage of custom port :- PORT=444 node index.js . If not given, use 5000
+
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+// Routes
+app.use("/api/v1/users", users);
+
+// app.use("/healthcheck", require("express-healthcheck")());
+
+app.listen(PORT, () => {
+  console.log(`Server started at port ${PORT}`);
+});
