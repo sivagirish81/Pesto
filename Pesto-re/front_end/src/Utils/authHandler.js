5674915c4c38bef0c09d22dbb99bdb9ba0208ab7
Siva Girish <sivagirish81@gmail.com>
2020-03-29 12:48:30 +0530

    Login - Logout - Dashboard -Done

---
+++ Pesto-re/front_end/src/Utils/authHandler.js
@@ -0,0 +1,14 @@
+
+import axios from "axios";
+
+const authHandler = token => {
+  if (token) {
+    // Apply authorization token to every request if logged in
+    axios.defaults.headers.common["Authorization"] = token;
+  } else {
+    // Delete auth header
+    delete axios.defaults.headers.common["Authorization"];
+  }
+};
+
+export default authHandler;
