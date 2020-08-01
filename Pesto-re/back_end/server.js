112c6c5b8c5a4aae5567c5d3e387b92ac74b9f7b
Siva Girish <sivagirish81@gmail.com>
2020-08-01 22:39:52 +0530

    .

--- Pesto-re/back_end/server.js
+++ Pesto-re/back_end/server.js
@@ -4,6 +4,7 @@ const users = require("./routes/api/v1/users");
 const pestos = require("./routes/api/v1/pestos");
 const admin = require("./routes/api/v1/admin");
 const app = express();
+const cors = require("cors");
 
 require('dotenv').config();
 
@@ -12,8 +13,10 @@ const connectDB = require("./config/db");
 // connect to MongoDB
 connectDB();
 
+
 // Init Body Parser middleware
 app.use(express.json({ extended: false }));
+app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 //allows usage of custom port :- PORT=444 node index.js . If not given, use 5000
