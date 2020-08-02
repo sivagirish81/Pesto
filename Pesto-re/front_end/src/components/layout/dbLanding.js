02aa3128f143010b5f5fa9e390a6cafa3258ca37
Siva Girish <sivagirish81@gmail.com>
2020-08-02 15:09:23 +0530

    Fetch

--- Pesto-re/front_end/src/components/layout/dbLanding.js
+++ Pesto-re/front_end/src/components/layout/dbLanding.js
@@ -55,11 +55,11 @@ class dbLanding extends Component {
           visible : "all"
         };
     console.log("userData : ",userData);
-    //this.props.pestoPost(userData);
-
+    this.props.pestoPost(userData);
+    
     const resp = fetch(`${API_URI}/api/v1/pestos/post`,{
       method: 'POST',
-      body: userData
+      body: json.stringify(userData)
     });
 
     window.location.reload(true);
